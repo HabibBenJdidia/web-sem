3be73ebf136e2c08665cdf781158cbd310a29bad
habib.ben.jdidia <benjdidiahabib2020@gmail.com>
2025-10-28 20:18:50 +0100

    Remove type display from transport cards, keep only category

--- Web-Semantique-Front/src/pages/TransportPublic.jsx
+++ Web-Semantique-Front/src/pages/TransportPublic.jsx
@@ -266,14 +266,6 @@ export function TransportPublic() {
                         <span className="detail-value">{categoryConfig.label}</span>
                       </div>
                       
-                      <div className="detail-item">
-                        <svg className="detail-icon" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-                        </svg>
-                        <span className="detail-label">Type:</span>
-                        <span className="detail-value">{transport.type}</span>
-                      </div>
-                      
                       {transport.empreinte && (
                         <div className="detail-item empreinte-item">
                           <svg className="detail-icon" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
